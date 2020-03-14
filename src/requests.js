c5ea2db7590b55e772fc9729a82b0e05d7815c46
coderus <coderusinbox@gmail.com>
2020-03-14 16:30:41 +0300

    Remove env host generation

--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,6 @@
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
-export const httpHost = window.ENV_HTTP_HOST ? window.ENV_HTTP_HOST:"localhost:8000";
-
-export const websocket = new ReconnectingWebSocket(`ws://${httpHost}/ws`);
+export const websocket = new ReconnectingWebSocket(`/ws`);
 
 export const status = (response) => {  
   if (response.status >= 200 && response.status < 300) {  
@@ -26,12 +24,12 @@ function futch(url, opts={}, onProgress) {
   });
 }
 
-export const uploadSize = (size) => fetch(`http://${httpHost}/updateSize`, {
+export const uploadSize = (size) => fetch(`/updateSize`, {
   method: "POST",
   body: new URLSearchParams(`fileSize=${size}`)
 }).then(status)
 
-export const uploadBinary = (file, onProgress) => futch(`http://${httpHost}/update`, {
+export const uploadBinary = (file, onProgress) => futch(`/update`, {
   method: "POST",
   body: file
 }, onProgress)
